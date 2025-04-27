5ed8f274962c53b5e3f409e443dfce9e81882ecd
pancakenut <2797100265@qq.com>
2025-04-27 09:55:10 +0800

    添加用户模块，包含管理员的用户名和密码

---
+++ server/auth/database/user.js
@@ -0,0 +1,9 @@
+// const password = createCryptoHash("admin");
+// console.log(password);
+module.exports = {
+  admin: {
+    password: "jGl25bVBBBW96Qi9Te4V37Fnqchz/Eu4qB9vKrRIqRg=",
+    username: "admin",
+    id: 1,
+  },
+};
